b114632276ae5a8faed24bea9de611d2723669c9
manabu niseki <zgok0079@gmail.com>
2015-10-11 07:17:06 +0900

    add rss-feed

--- app.rb
+++ app.rb
@@ -1,9 +1,18 @@
 require 'sinatra'
 
+configure do
+  $LOAD_PATH.unshift("#{File.dirname(__FILE__)}/lib")
+  Dir.glob("#{File.dirname(__FILE__)}/lib/*.rb") do |lib|
+    require File.basename(lib, '.*')
+  end
+end
+
 get '/' do
   File.read(File.join('public', 'index.html'))
 end
 
 get '/rss' do
+  content_type 'application/xml'
 
+  TheNoises::Podcast.new.rss
 end
